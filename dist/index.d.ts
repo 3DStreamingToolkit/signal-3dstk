3103904adfcbe112e229b7ee55de393fdf6a0315
Conrad Mitchell <t-comitc@microsoft.com>
2019-07-23 15:54:50 -0700

    removing dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-declare const appCreator: any;
-declare const app: any;
--- dist/lib/modules.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Publisher = require("webrtc-signal-http-publisher");
-//# sourceMappingURL=modules.js.map
